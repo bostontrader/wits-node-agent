3d192d2ec08f03a2945dd8f76852974b8df9288a
Thomas Radloff <bostontrader@gmail.com>
2016-09-19 09:17:18 +0800

    Add configuration system.

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-var net = require('net');
+let config = require('config')
+console.log('Using configuration %s',config.get('configName'))
+let net = require('net')
 
-var HOST = '192.168.1.104';
-var PORT = 8000;
+let client = new net.Socket()
+let wits_central_host = config.get('wits_central_host')
+let wits_central_tcp_port = config.get('wits_central_tcp_port')
+client.connect(wits_central_tcp_port, wits_central_host, function() {
 
-var client = new net.Socket();
-client.connect(PORT, HOST, function() {
-
-    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-    // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
-    client.write('{"who": "i am spartacus", "id": "5f18453d-1907-48bc-abd2-ab6c24bc197d"}')
-    client.write("\n");
-
-});
+  console.log('CONNECTED TO: ' + wits_central_host + ':' + wits_central_tcp_port)
+  client.write('{"who": "i am spartacus", "id": "5f18453d-1907-48bc-abd2-ab6c24bc197d"}')
+  client.write("\n");
+})
 
 client.on('data', function(data) {
-    console.log('DATA: ' + data);
-    client.destroy();
+  console.log('DATA: ' + data)
+  client.destroy()
 })
 
 client.on('close', function() {
-    console.log('Connection closed');
+  console.log('Connection closed')
 })
